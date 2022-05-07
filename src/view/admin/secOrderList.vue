1010a1ea6b7637c98757674b771bc4edfd432ba0
wujunqing <wujunqing@limayao.com>
2022-05-08 06:06:27 +0800

    feat: secOrderList

--- src/view/admin/secOrderList.vue
+++ src/view/admin/secOrderList.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="user-list">
     <div class="user-table">
-      <el-table :data="tableData" class="box">
-        <el-table-column prop="id" label="ID"/>
-        <el-table-column prop="username" label="所属用户"/>
-        <el-table-column prop="goodsName" label="商品名称"/>
-        <el-table-column prop="price" label="总价"/>
-        <el-table-column prop="orderStatus" label="状态"/>
+      <el-table :data="orderList.list" class="box">
+        <el-table-column prop="Id" label="ID" width="170px"/>
+        <el-table-column prop="Username" label="所属用户"/>
+        <el-table-column prop="GoodsName" label="商品名称"/>
+        <el-table-column prop="Price" label="总价"/>
+        <el-table-column prop="OrderStatus" label="状态"/>
         <el-table-column prop="do" label="操作"/>
       </el-table>
     </div>
@@ -14,8 +14,38 @@
 </template>
 
 <script>
+import api from "@/utils/request";
+
 export default {
-  name: "secOrderList"
+  name: "secOrderList",
+  mounted() {
+    this.getSecOrderList()
+  },
+  methods: {
+    getSecOrderList() {
+      api({
+        method: "GET",
+        url: "http://localhost:8082/api/v1/secOrder/querySecOrderPage",
+        params: {
+          pageNum:0,
+          pageSize:15
+        },
+        data:{}
+      }).then(info => {
+        this.orderList.count=info.data.data.count
+        this.orderList.list=info.data.data.list
+      });
+      console.log(this.Info)
+    }
+  },
+  data(){
+    return{
+      orderList:{
+        count:0,
+        list:[]
+      }
+    }
+  }
 }
 </script>
 
